a7b500c9c985e1029acdc0cbe476ea0eced85c9c
Nick Xie <nickxie.me@gmail.com>
2018-02-10 16:28:46 -0500

    named gulp task wrong

--- gulpfile.js
+++ gulpfile.js
@@ -55,6 +55,6 @@ gulp.task('watch', function() {
 
 gulp.task('build', ['js']);
 
-gulp.task('heroku:production', ['build']);
+gulp.task('heroku:', ['build']);
 
 gulp.task('default', ['watch', 'js']);
